d98628fdaf2a8ce01f7d92fca662b3f41ce76150
HP4k1h5 <robertwalks@gmail.com>
2018-08-06 14:51:24 -0400

    modified:   marketData.js 	modified:   style.css

--- marketData.js
+++ marketData.js
@@ -2,24 +2,33 @@ let mktDiv = document.createElement('div')
 mktDiv.setAttribute('class', 'market')
 mktDiv.id = 'market'
 document.getElementById('main').appendChild(mktDiv)
-
 fetchMovers()
+setInterval(fetchMovers, 10000)
 
 function fetchMovers(){
-  let base = 'https://api.iextrading.com/1.0/stock/'
-  //let base = 'https://api.iextrading.com/1.0/stock/market/list/'
-  //let queries = ['losers', 'gainer']
-  let queries = ['aapl', 'msft']
-  function moverCalls(){
-    return queries.map(q => {
-      return new Promise((res, rej) =>
-        res(fetch(base + q))
-      )
+  let base = 'https://api.iextrading.com/1.0/stock/market/list/'
+  let queries = ['losers', 'gainers']
+
+  Promise.all(queries.map(q => 
+    fetch(base + q))).then(resp =>
+      Promise.all(resp.map(res => 
+        res.json()
+      ))
+  ).then(r => {
+  mktDiv.childNodes.forEach(c => c.parentNode.removeChild(c))
+    let ul = document.createElement('ul')
+    r.forEach(o => { 
+      o.forEach(p => {
+        let li = document.createElement('li')
+        let txt = `${p.symbol}: ${p.changePercent*100}%`
+        txt = document.createTextNode(txt)
+        li.appendChild(txt)
+        p.change >= 0 ? 
+          li.setAttribute('class', 'green')
+          : li.setAttribute('class', 'red')
+        ul.appendChild(li)
+      })
+      mktDiv.appendChild(ul)
     })
-  }
-  let promises = moverCalls()
-  console.log(promises)
-  Promise.all(promises).then(p =>
-    console.log(p.json())
-  )
+  })
 }

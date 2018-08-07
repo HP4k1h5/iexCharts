09bf63199822e88d55b3dcf3935e51c58aa2fa14
HP4k1h5 <robertwalks@gmail.com>
2018-08-07 11:57:44 -0400

    modified:   ChartBox.js added news source

--- ChartBox.js
+++ ChartBox.js
@@ -64,8 +64,8 @@ function fetchNews(meta){
     .then(j => j.json())
     .then(d => {
       let txt = d.reduce((a,n) => 
-        a + n.datetime.slice(0,16) + ': ' 
-        + '<b>' + n.headline + '</b>' 
+        a + n.datetime.slice(0,16) + ': ' + n.source
+        + '<br><b>' + n.headline + '</b>' 
         + '<br>' + n.summary + ':' 
         + '<a href=' + n.url + '>' + 'link' + '</a>' 
         + '<br>' + '•••'
